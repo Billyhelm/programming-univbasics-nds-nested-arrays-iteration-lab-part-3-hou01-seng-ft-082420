ea2b21087b267f5758b996d66a3de3d53dfeb251
Billy Helm <billyhelm88@gmail.com>
2020-07-27 19:15:01 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,5 +1,10 @@
 def join_nested_strings(src)
+  outside = 0
+  sentence
   while outside < src.count do
-  
+  inside = 0 
+    while inside < src[outside].count do 
+      
   end
+  outside += 1 
 end
